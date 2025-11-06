0a412be09dae5b72089cd0f23c039599d322490f
pauzhetsky <kazancev.alex2020@gmail.com>
2025-11-06 16:42:18 +0300

    fix: change email

--- src/components/ContactModal/ContactModal.tsx
+++ src/components/ContactModal/ContactModal.tsx
@@ -1,35 +1,35 @@
-import React from 'react'
-import styles from './ContactModal.module.css'
+import React from "react";
+import styles from "./ContactModal.module.css";
 
 interface ContactModalProps {
-	isOpen: boolean
-	onClose: () => void
+  isOpen: boolean;
+  onClose: () => void;
 }
 
 const ContactModal: React.FC<ContactModalProps> = ({ isOpen, onClose }) => {
-	if (!isOpen) return null
+  if (!isOpen) return null;
 
-	return (
-		<div className={`${styles.modal} no-print`} onClick={onClose}>
-			<div className={styles.modalContent} onClick={(e) => e.stopPropagation()}>
-				<button className={styles.closeModal} onClick={onClose}>
-					&times;
-				</button>
-				<a href='https://t.me/KazantsevJS' className={styles.modalContactItem}>
-					<span className={styles.itemTitle}>Telegram</span>
-				</a>
-				<a href='https://wa.me/79960302435' className={styles.modalContactItem}>
-					<span className={styles.itemTitle}>WhatsApp</span>
-				</a>
-				<a
-					href='mailto:ale.kazantsev@mail.astondevs.ru'
-					className={styles.modalContactItem}
-				>
-					<span className={styles.itemTitle}>Email</span>
-				</a>
-			</div>
-		</div>
-	)
-}
+  return (
+    <div className={`${styles.modal} no-print`} onClick={onClose}>
+      <div className={styles.modalContent} onClick={(e) => e.stopPropagation()}>
+        <button className={styles.closeModal} onClick={onClose}>
+          &times;
+        </button>
+        <a href="https://t.me/KazantsevJS" className={styles.modalContactItem}>
+          <span className={styles.itemTitle}>Telegram</span>
+        </a>
+        <a href="https://wa.me/79960302435" className={styles.modalContactItem}>
+          <span className={styles.itemTitle}>WhatsApp</span>
+        </a>
+        <a
+          href="mailto:kazancev.alex2020@gmail.com"
+          className={styles.modalContactItem}
+        >
+          <span className={styles.itemTitle}>Email</span>
+        </a>
+      </div>
+    </div>
+  );
+};
 
-export default ContactModal
+export default ContactModal;
